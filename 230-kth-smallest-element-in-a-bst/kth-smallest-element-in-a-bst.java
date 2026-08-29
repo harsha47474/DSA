f2bcb2d2ec46515d531616aa5336b6453798679e
Harsha <harsha1127achu@gmail.com>
2026-08-29 13:11:55 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (17.63%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -13,22 +13,44 @@
  *     }
  * }
  */
+// ======================== PRIORITY QUEUE METHOD ============================
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         fillPQ(root, pq);
+//         for (int i = 1; i < k; i++) {
+//             pq.poll();
+//         }
+
+//         return pq.peek();
+//     }
+
+//     public void fillPQ(TreeNode root, PriorityQueue<Integer> pq) {
+//         if (root == null)
+//             return;
+//         pq.offer(root.val);
+//         fillPQ(root.left, pq);
+//         fillPQ(root.right, pq);
+//     }
+// }
+
+// ========================== INRODER METHOD ===================================
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        fillPQ(root, pq);
-        for (int i = 1; i < k; i++) {
-            pq.poll();
-        }
-
-        return pq.peek();
+    int ans = 0;
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k){
+        findInorder(root, k);
+        return ans;
     }
+    public void findInorder(TreeNode root, int k){
+        if(root == null) return;
 
-    public void fillPQ(TreeNode root, PriorityQueue<Integer> pq) {
-        if (root == null)
+        findInorder(root.left, k);
+        count++;
+        if(count == k){
+            ans = root.val;
             return;
-        pq.offer(root.val);
-        fillPQ(root.left, pq);
-        fillPQ(root.right, pq);
+        }
+        findInorder(root.right, k);
     }
 }
