e092d7bef1510d2ff6246d058cc170e65b9341b0
Harsha <harsha1127achu@gmail.com>
2026-08-29 12:16:43 +0530

    Time: 0 ms (100%), Space: 47.2 MB (16.2%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -13,13 +13,29 @@
  *     }
  * }
  */
+// =========================== DFS TRAVERSAL ==============================
+// class Solution {
+//     public TreeNode searchBST(TreeNode root, int val) {
+//         if(root == null) return null;
+//         TreeNode temp = root;
+//         if (root.val == val) return root;
+//         else if (root.val > val) temp = searchBST(root.left, val);
+//         else if (root.val < val) temp = searchBST(root.right, val);
+//         return temp;
+//     }
+// }
+
+// ============================ BFS TRAVERSAL =============================
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return null;
-        TreeNode temp = root;
-        if(root.val == val) return root;
-        else if(root.val > val) temp = searchBST(root.left, val);
-        else if(root.val < val) temp = searchBST(root.right, val);
-        return temp;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            if(temp.val == val) return temp;
+            else if(temp.val > val && temp.left != null) queue.offer(temp.left);
+            else if(temp.val < val && temp.right != null) queue.offer(temp.right);
+        }
+        return null;
     }
 }
